d517537fa7c12a33f0ddcebaf9699f7cd98a12f6
Andres <ajtaboada04@gmail.com>
2024-10-08 23:40:18 +0200

    inor code readability changes

--- iebank_api/models.py
+++ iebank_api/models.py
@@ -6,23 +6,23 @@ class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
     account_number = db.Column(db.String(20), nullable=False, unique=True)
+    # add country to account
+    country = db.Column(db.String(64), nullable=False, default="Spain") 
     balance = db.Column(db.Float, nullable=False, default = 0.0)
     currency = db.Column(db.String(1), nullable=False, default="€")
     status = db.Column(db.String(10), nullable=False, default="Active")
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    # add country to account
-    country = db.Column(db.String(3), nullable=False, default="ESP") 
-
+    
     def __repr__(self):
         return '<Event %r>' % self.account_number
 
     def __init__(self, name, currency, country):
         self.name = name
         self.account_number = ''.join(random.choices(string.digits, k=20))
+        # add country to account constructor
+        self.country = country
         self.currency = currency
         self.balance = 0.0
         self.status = "Active"
-        # add country to account constructor
-        self.country = country
         
         
